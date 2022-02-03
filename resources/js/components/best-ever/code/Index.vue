cfdca18cee80cc9df3251160882565d938aa4015
khencel <khenneth.alaiza@gmail.com>
2022-02-03 12:28:19 +0800

    qimen v3

--- resources/js/components/best-ever/code/Index.vue
+++ resources/js/components/best-ever/code/Index.vue
@@ -2,21 +2,24 @@
     <div>
         <notifications group="notification" position="bottom right" />
         <div class="w-100 mt-2">
-            <button :disabled="is_active == 1?false:true" @click="createCode" class="btn btn-success float-right mb-2">Generate Code</button>
+            <button @click="createCode" class="btn btn-success float-right mb-2">Generate Code</button>
         </div>
         <div>
             <table class="table table-hover">
                 <thead>
                     <th>#</th>
+                    <th>Description</th>
                     <th>Code</th>
                     <th>No. of Users</th>
                     <th>Expiration Date</th>
                     <th>Status</th>
+                    <th>Action</th>
                     <!-- <th>Action</th> -->
                 </thead>
                 <tbody>
                     <tr v-for="(code, index) in codes" :key="index">
                         <td>{{index+1}}</td>
+                        <td>{{code.description}}</td>
                         <td>{{code.code}}</td>
                         <td>{{code.numb_of_users}}</td>
                         <td>
@@ -25,6 +28,9 @@
                         <td>
                             <span v-if="code.status == 0" class="badge badge-danger p-2">Expired</span>
                             <span v-if="code.status == 1" class="badge badge-success p-2">Active</span>
+                        </td>
+                        <td>
+                            <button v-if="code.status == 1" @click="showCode(code.id)" class="btn btn-primary">Edit</button>
                         </td>
                         <!-- <td><button :disabled="code.status == 0?true:false" class="btn btn-danger">Deactivate</button></td> -->
                     </tr>
@@ -44,6 +50,20 @@
                 </div>
                 <div class="modal-body">
                     <div class="row">
+
+                        <div class="col-md-10">
+                            <strong>
+                                <label for="">Code</label>
+                            </strong>
+                            <input type="text" class="form-control" placeholder="Code here(optional)..." v-model="form.code">
+                        </div>
+
+                        <div class="col-md-10">
+                            <strong>
+                                <label for="">Description</label>
+                            </strong>
+                            <textarea class="form-control" placeholder="Description here..." cols="30" v-model="form.description" rows="10"></textarea>
+                        </div>
                         <div class="col-md-10">
                             <strong>
                                 <label for="">Expiration Date</label>
@@ -70,11 +90,15 @@
                 codes:{},
                 is_active:'',
                 form:new Form({
+                    id:'',
                     year: new Date().getFullYear(),
                     month: new Date().getMonth()+1,
                     day: new Date().getDate(),
                     time: new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds(),
-                    exp_date:''
+                    exp_date:'',
+                    description:'',
+                    code:'',
+                    numb_of_users:''
                 }),
             }
         },
@@ -89,17 +113,19 @@
                 this.is_active = res.data;
             },
             createCode(){
-                if(this.is_active == 1){
-                    $('#modalCode').modal('show');
-                }
+                this.form.code = '';
+                this.form.description = '';
+                this.form.exp_date = '';
+                $('#modalCode').modal('show');
             },
             storeCode(){
                 this.form.post(window.besteverLive+'/api/generate-code/store')
                 .then(res => {
-                    console.log(res.data);
+                    
                     if(res.data == "Invalid Expiration Date"){
                         alert(res.data);
                     }else{
+                        console.log(res.data);
                         this.index();
                         $('#modalCode').modal('hide');
                         this.$notify({
@@ -110,6 +136,17 @@
                         });
                     }
                 })
+            },
+
+            async showCode(id){
+                this.form.id = id;
+                $('#modalCode').modal('show');
+                const res = await axios.get(window.besteverLive+'/api/code/show/'+id);
+                this.form.id = res.data.id;
+                this.form.description = res.data.description;
+                this.form.code = res.data.code;
+                this.form.exp_date = res.data.exp_date.split(' ')[0];
+                this.form.numb_of_users = res.data.numb_of_users
             }
         },
         mounted() {
